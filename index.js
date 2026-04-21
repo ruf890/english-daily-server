bc6bf892a2a15f08ce60a9dfa13ab3d5251fc238
ruf890 <ninfe@msn.com>
2026-04-21 23:28:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 const GEMINI_KEY = process.env.GEMINI_API_KEY;
-const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${GEMINI_KEY}`;
-
+const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${GEMINI_KEY}`;
 app.post("/api/generate", async (req, res) => {
   try {
     const { prompt } = req.body;
